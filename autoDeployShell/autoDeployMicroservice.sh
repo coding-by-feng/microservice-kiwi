01ca75484214b1354683251e3dd8d585c4d2cb8f
coding-by-feng <1215587087@qq.com>
2020-05-22 00:25:50 +0800

    增加自动部署的shell脚本，暂时不使用jenkins。

---
+++ autoDeployShell/autoDeployMicroservice.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd /root/microservice-kiwi/
+
+git reset --hard master
+git pull https://github.com/coding-by-feng/microservice-kiwi.git/
+
+mvn clean install -Dmaven.test.skip=true
+
+mv -f kiwi-eureka/Dockerfile /root/docker/kiwi/eureka/
+mv -f kiwi-config/Dockerfile /root/docker/kiwi/config/
+mv -f kiwi-upms/kiwi-upms-biz/Dockerfile /root/docker/kiwi/upms/
+mv -f kiwi-word/kiwi-word-biz/Dockerfile /root/docker/kiwi/word/
+mv -f kiwi-word/kiwi-word-crawler/Dockerfile /root/docker/kiwi/crawler/
+mv -f kiwi-auth/Dockerfile /root/docker/kiwi/auth/
+mv -f kiwi-gateway/Dockerfile /root/docker/kiwi/gate/
+
+mv -f kiwi-eureka/target/kiwi-eureka-1.0-SNAPSHOT.jar /root/docker/kiwi/eureka/
+mv -f kiwi-config/target/kiwi-config-1.0-SNAPSHOT.jar /root/docker/kiwi/config/
+mv -f kiwi-upms/kiwi-upms-biz/target/kiwi-upms-biz-1.0-SNAPSHOT.jar /root/docker/kiwi/upms/
+mv -f kiwi-auth/target/kiwi-auth-1.0-SNAPSHOT.jar /root/docker/kiwi/auth/
+mv -f kiwi-gateway/target/kiwi-gateway-1.0-SNAPSHOT.jar /root/docker/kiwi/gate/
+mv -f kiwi-word/kiwi-word-biz/target/kiwi-word-biz-1.0-SNAPSHOT.jar /root/docker/kiwi/word/
+mv -f kiwi-word/kiwi-word-crawler/target/kiwi-word-crawler-1.0-SNAPSHOT.jar /root/docker/kiwi/crawler/
+
+/root/docker/kiwi/autoDeploy.sh

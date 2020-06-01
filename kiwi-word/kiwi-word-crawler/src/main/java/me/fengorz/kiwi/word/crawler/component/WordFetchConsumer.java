e53635233f4f711430f3503cb436af1cfd182bf1
coding-by-feng <1215587087@qq.com>
2020-06-01 23:04:46 +0800

    fix bug

--- kiwi-word/kiwi-word-crawler/src/main/java/me/fengorz/kiwi/word/crawler/component/WordFetchConsumer.java
+++ kiwi-word/kiwi-word-crawler/src/main/java/me/fengorz/kiwi/word/crawler/component/WordFetchConsumer.java
@@ -76,7 +76,7 @@ public class WordFetchConsumer {
                 wordFetchService.work(wordMessageDTO);
             });
         } finally {
-            lock.unlock();
+            this.lock.unlock();
         }
     }
 

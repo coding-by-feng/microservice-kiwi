29ba075486906ba6729df1d36458339d7fd921db
coding-by-feng <1215587087@qq.com>
2020-06-03 23:14:47 +0800

    optimize

--- autoDeployShell/autoDeployMicroservice.sh
+++ autoDeployShell/autoDeployMicroservice.sh
@@ -4,26 +4,46 @@ killall autoCheckService.sh
 
 cd /root/microservice-kiwi/
 
+rm -rf ./*
+
 git reset --hard master
 git pull https://github.com/coding-by-feng/microservice-kiwi.git/
 
+cd kiwi-word
+mvn clean install -Dmaven.test.skip=true
+cd ..
+cd kiwi-eureka/
+mvn clean install -Dmaven.test.skip=true
+cd ..
+cd kiwi-config/
+mvn clean install -Dmaven.test.skip=true
+cd ..
+cd kiwi-gateway/
+mvn clean install -Dmaven.test.skip=true
+cd ..
+cd kiwi-auth/
+mvn clean install -Dmaven.test.skip=true
+cd ..
+cd kiwi-upms/
 mvn clean install -Dmaven.test.skip=true
 
-mv -f /root/microservice-kiwi/kiwi-eureka/Dockerfile /root/docker/kiwi/eureka/
-mv -f /root/microservice-kiwi/kiwi-config/Dockerfile /root/docker/kiwi/config/
-mv -f /root/microservice-kiwi/kiwi-upms/kiwi-upms-biz/Dockerfile /root/docker/kiwi/upms/
-mv -f /root/microservice-kiwi/kiwi-word/kiwi-word-biz/Dockerfile /root/docker/kiwi/word/
-mv -f /root/microservice-kiwi/kiwi-word/kiwi-word-crawler/Dockerfile /root/docker/kiwi/crawler/
-mv -f /root/microservice-kiwi/kiwi-auth/Dockerfile /root/docker/kiwi/auth/
-mv -f /root/microservice-kiwi/kiwi-gateway/Dockerfile /root/docker/kiwi/gate/
+cd /root/microservice-kiwi/
 
-mv -f /root/microservice-kiwi/kiwi-eureka/target/kiwi-eureka-1.0-SNAPSHOT.jar /root/docker/kiwi/eureka/
-mv -f /root/microservice-kiwi/kiwi-config/target/kiwi-config-1.0-SNAPSHOT.jar /root/docker/kiwi/config/
-mv -f /root/microservice-kiwi/kiwi-upms/kiwi-upms-biz/target/kiwi-upms-biz-1.0-SNAPSHOT.jar /root/docker/kiwi/upms/
-mv -f /root/microservice-kiwi/kiwi-auth/target/kiwi-auth-1.0-SNAPSHOT.jar /root/docker/kiwi/auth/
-mv -f /root/microservice-kiwi/kiwi-gateway/target/kiwi-gateway-1.0-SNAPSHOT.jar /root/docker/kiwi/gate/
-mv -f /root/microservice-kiwi/kiwi-word/kiwi-word-biz/target/kiwi-word-biz-1.0-SNAPSHOT.jar /root/docker/kiwi/word/
-mv -f /root/microservice-kiwi/kiwi-word/kiwi-word-crawler/target/kiwi-word-crawler-1.0-SNAPSHOT.jar /root/docker/kiwi/crawler/
+mv -f kiwi-eureka/Dockerfile /root/docker/kiwi/eureka/
+mv -f kiwi-config/Dockerfile /root/docker/kiwi/config/
+mv -f kiwi-upms/kiwi-upms-biz/Dockerfile /root/docker/kiwi/upms/
+mv -f kiwi-word/kiwi-word-biz/Dockerfile /root/docker/kiwi/word/
+mv -f kiwi-word/kiwi-word-crawler/Dockerfile /root/docker/kiwi/crawler/
+mv -f kiwi-auth/Dockerfile /root/docker/kiwi/auth/
+mv -f kiwi-gateway/Dockerfile /root/docker/kiwi/gate/
+
+mv -f ~/.m2/repository/me/fengorz/kiwi-eureka/1.0-SNAPSHOT/kiwi-eureka-1.0-SNAPSHOT.jar /root/docker/kiwi/eureka/
+mv -f ~/.m2/repository/me/fengorz/kiwi-config/1.0-SNAPSHOT/kiwi-config-1.0-SNAPSHOT.jar /root/docker/kiwi/config/
+mv -f ~/.m2/repository/me/fengorz/kiwi-upms-biz/1.0-SNAPSHOT/kiwi-upms-biz-1.0-SNAPSHOT.jar /root/docker/kiwi/upms/
+mv -f ~/.m2/repository/me/fengorz/kiwi-auth/1.0-SNAPSHOT/kiwi-auth-1.0-SNAPSHOT.jar /root/docker/kiwi/auth/
+mv -f ~/.m2/repository/me/fengorz/kiwi-gateway/1.0-SNAPSHOT/kiwi-gateway-1.0-SNAPSHOT.jar /root/docker/kiwi/gate/
+mv -f ~/.m2/repository/me/fengorz/kiwi-word-biz/1.0-SNAPSHOT/kiwi-word-biz-1.0-SNAPSHOT.jar /root/docker/kiwi/word/
+mv -f ~/.m2/repository/me/fengorz/kiwi-word-crawler/1.0-SNAPSHOT/kiwi-word-crawler-1.0-SNAPSHOT.jar /root/docker/kiwi/crawler/
 
 /root/docker/kiwi/autoDeploy.sh
 

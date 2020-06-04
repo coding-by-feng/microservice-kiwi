b352556ba255dfada6460e4a42ba0954c415dce9
coding-by-feng <1215587087@qq.com>
2020-06-04 14:38:47 +0800

    add deploy notes

--- autoDeployShell/envReady.sh
+++
@@ -1,6 +0,0 @@
-# rabbitmq
-docker run -d -m 200M --hostname my-rabbit --name some-rabbit -p 15555:15672 rabbitmq:management
-
-# nginx
-docker run -p 16818:80 -v /root/docker/ui/dist/:/usr/share/nginx/html -v /root/docker/ui/nginx/:/etc/nginx --name=kiwi-ui -it kiwi-ui:1.0
-

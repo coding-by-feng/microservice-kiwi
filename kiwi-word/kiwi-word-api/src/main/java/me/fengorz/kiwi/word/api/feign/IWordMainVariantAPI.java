2b4d4349c099feb8e10ae56c69d3b7f94d547004
coding-by-feng <1215587087@qq.com>
2020-06-14 16:30:28 +0800

    optimize

--- kiwi-word/kiwi-word-api/src/main/java/me/fengorz/kiwi/word/api/feign/IWordMainVariantAPI.java
+++ kiwi-word/kiwi-word-api/src/main/java/me/fengorz/kiwi/word/api/feign/IWordMainVariantAPI.java
@@ -18,12 +18,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import me.fengorz.kiwi.common.api.R;
 import me.fengorz.kiwi.word.api.common.WordConstants;
+import me.fengorz.kiwi.word.api.feign.fallback.WordMainVariantAPIFallback;
 
 /**
  * @Author zhanshifeng
  * @date 2020-05-24 01:40:36
  */
-@FeignClient(contextId = "remoteWordMainVariantService", value = WordConstants.KIWI_WORD_BIZ)
+@FeignClient(contextId = "remoteWordMainVariantService", value = WordConstants.KIWI_WORD_BIZ,
+    fallbackFactory = WordMainVariantAPIFallback.class)
 public interface IWordMainVariantAPI {
 
     String WORD_MAIN_VARIANT = "/word/main/variant";

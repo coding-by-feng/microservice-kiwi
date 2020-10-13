48f6b2c95b207e206967cda0c35abb11c41938ae
coding-by-feng <1215587087@qq.com>
2020-10-13 11:16:46 +0800

    Do nothing.

--- kiwi-deploy/autoDeployMicroservice.sh
+++ kiwi-deploy/autoDeployMicroservice.sh
@@ -4,40 +4,17 @@ killall autoCheckService.sh
 
 cd ~/microservice-kiwi/
 
-#rm -rf ./*
-
-git reset --hard master
-git pull git@github.com/coding-by-feng/microservice-kiwi.git/
+git pull
 
 mvn clean install -Dmaven.test.skip=true
 
-#cd kiwi-word
-#mvn clean install -Dmaven.test.skip=true
-#cd ..
-#cd kiwi-eureka/
-#mvn clean install -Dmaven.test.skip=true
-#cd ..
-#cd kiwi-config/
-#mvn clean install -Dmaven.test.skip=true
-#cd ..
-#cd kiwi-gateway/
-#mvn clean install -Dmaven.test.skip=true
-#cd ..
-#cd kiwi-auth/
-#mvn clean install -Dmaven.test.skip=true
-#cd ..
-#cd kiwi-upms/
-#mvn clean install -Dmaven.test.skip=true
-
-#cd ~/microservice-kiwi/
-
-mv -f kiwi-eureka/Dockerfile ~/docker/kiwi/eureka/
-mv -f kiwi-config/Dockerfile ~/docker/kiwi/config/
-mv -f kiwi-upms/kiwi-upms-biz/Dockerfile ~/docker/kiwi/upms/
-mv -f kiwi-word/kiwi-word-biz/Dockerfile ~/docker/kiwi/word/
-mv -f kiwi-word/kiwi-word-crawler/Dockerfile ~/docker/kiwi/crawler/
-mv -f kiwi-auth/Dockerfile ~/docker/kiwi/auth/
-mv -f kiwi-gateway/Dockerfile ~/docker/kiwi/gate/
+mv -f ~/microservice-kiwi/kiwi-eureka/Dockerfile ~/docker/kiwi/eureka/
+mv -f ~/microservice-kiwi/kiwi-config/Dockerfile ~/docker/kiwi/config/
+mv -f ~/microservice-kiwi/kiwi-upms/kiwi-upms-biz/Dockerfile ~/docker/kiwi/upms/
+mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/Dockerfile ~/docker/kiwi/word/
+mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-crawler/Dockerfile ~/docker/kiwi/crawler/
+mv -f ~/microservice-kiwi/kiwi-auth/Dockerfile ~/docker/kiwi/auth/
+mv -f ~/microservice-kiwi/kiwi-gateway/Dockerfile ~/docker/kiwi/gate/
 
 mv -f ~/.m2/repository/me/fengorz/kiwi-eureka/1.0-SNAPSHOT/kiwi-eureka-1.0-SNAPSHOT.jar ~/docker/kiwi/eureka/
 mv -f ~/.m2/repository/me/fengorz/kiwi-config/1.0-SNAPSHOT/kiwi-config-1.0-SNAPSHOT.jar ~/docker/kiwi/config/
@@ -49,7 +26,7 @@ mv -f ~/.m2/repository/me/fengorz/kiwi-word-crawler/1.0-SNAPSHOT/kiwi-word-crawl
 
 ~/autoDeploy.sh
 
-echo sleep 300
-sleep 100s
+echo sleep 200
+sleep 200s
 
-nohup ~/autoCheckService.sh  >~/autoCheck.log 2>&1 &
+# nohup ~/autoCheckService.sh  >~/autoCheck.log 2>&1 &

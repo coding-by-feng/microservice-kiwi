a20885844804f502d13ef0d1c19bc97aa42482ff
coding-by-feng <1215587087@qq.com>
2020-10-19 13:42:04 +0800

    Bind code verify in oneClickRegister.

--- kiwi-common/kiwi-common-sdk/src/main/java/me/fengorz/kiwi/common/sdk/config/FilterIgnorePropertiesConfig.java
+++ kiwi-common/kiwi-common-sdk/src/main/java/me/fengorz/kiwi/common/sdk/config/FilterIgnorePropertiesConfig.java
@@ -16,15 +16,14 @@
 
 package me.fengorz.kiwi.common.sdk.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import lombok.Data;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author zhanshifeng
@@ -42,5 +41,7 @@ public class FilterIgnorePropertiesConfig {
     private List<String> urls = new ArrayList<>();
 
     private List<String> userNames = new ArrayList<>();
+
+    private List<String> codeVerifyChannels = new ArrayList<>();
 
 }

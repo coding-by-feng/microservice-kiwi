af49bc4f1754a60770454b60b00aa7a0b1992402
coding-by-feng <1215587087@qq.com>
2020-11-09 20:16:15 +0800

    Fixing Bug.

--- kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/service/operate/impl/CleanerService.java
+++ kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/service/operate/impl/CleanerService.java
@@ -93,7 +93,9 @@ public class CleanerService implements ICleanerService {
             //     // 如果所查单词和单词的原型不同的话
             //     list.addAll(mainService.list(derivation, WordCrawlerConstants.QUEUE_INFO_TYPE_WORD));
             // }
-            List<WordMainDO> list = new LinkedList<>(mainService.listDirtyData(queue.getWordId()));
+            // TODO ZSF 后面还是要还原为listDirtyData
+            // List<WordMainDO> list = new LinkedList<>(mainService.listDirtyData(queue.getWordId()));
+            List<WordMainDO> list = new LinkedList<>(mainService.list(Wrappers.<WordMainDO>lambdaQuery().eq(WordMainDO::getWordName, wordName)));
             if (KiwiCollectionUtils.isEmpty(list)) {
                 return;
             }

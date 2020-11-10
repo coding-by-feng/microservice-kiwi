3b3c4228edc61b3d1ceecba6af3f8db808b01d54
coding-by-feng <1215587087@qq.com>
2020-11-10 14:51:52 +0800

    Modifying shell script.

--- kiwi-deploy/autoDeployMicroservice.sh
+++ kiwi-deploy/autoDeployMicroservice.sh
@@ -2,6 +2,17 @@
 
 killall autoCheckService.sh
 
+echo "stop container beginning"
+
+docker stop `docker ps -a| grep kiwi-eureka | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-config | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-upms | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-auth | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-gate | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-word-biz | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-word-biz-crawler | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-crawler | awk '{print $1}' `
+
 rm -rf ~/docker/kiwi/eureka/logs/*
 rm -rf ~/docker/kiwi/config/logs/*
 rm -rf ~/docker/kiwi/upms/logs/*

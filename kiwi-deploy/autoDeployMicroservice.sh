e71afbd9b29959c55437df38d78bc41ef2dc7dfd
coding-by-feng <1215587087@qq.com>
2020-11-10 16:16:42 +0800

    Multiplying instances.

--- kiwi-deploy/autoDeployMicroservice.sh
+++ kiwi-deploy/autoDeployMicroservice.sh
@@ -4,14 +4,14 @@ killall autoCheckService.sh
 
 echo "stop container beginning"
 
-docker stop `docker ps -a| grep kiwi-eureka | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-config | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-crawler | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-word-biz-crawler | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-word-biz | awk '{print $1}' `
 docker stop `docker ps -a| grep kiwi-upms | awk '{print $1}' `
 docker stop `docker ps -a| grep kiwi-auth | awk '{print $1}' `
 docker stop `docker ps -a| grep kiwi-gate | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-word-biz | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-word-biz-crawler | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-crawler | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-config | awk '{print $1}' `
+docker stop `docker ps -a| grep kiwi-eureka | awk '{print $1}' `
 
 rm -rf ~/docker/kiwi/eureka/logs/*
 rm -rf ~/docker/kiwi/config/logs/*

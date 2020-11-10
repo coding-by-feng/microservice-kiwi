17d8e6f3b26c1f66194d46732a6011d21d69abfb
coding-by-feng <1215587087@qq.com>
2020-11-10 14:51:52 +0800

    Modifying shell script.

--- kiwi-deploy/autoDeploy.sh
+++ kiwi-deploy/autoDeploy.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-echo "stop container beginning"
-
-docker stop `docker ps -a| grep kiwi-eureka | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-config | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-upms | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-auth | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-gate | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-word-biz | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-word-biz-crawler | awk '{print $1}' `
-docker stop `docker ps -a| grep kiwi-crawler | awk '{print $1}' `
-
 echo "delete container beginning"
 
 docker rm   `docker ps -a| grep kiwi-eureka | awk '{print $1}' `

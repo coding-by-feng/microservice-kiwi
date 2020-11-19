3db4eb659283f7845af3c16371165f0840a48703
coding-by-feng <1215587087@qq.com>
2020-11-19 21:57:50 +0800

    Add store into es.

--- kiwi-word/kiwi-word-api/src/main/java/me/fengorz/kiwi/word/api/feign/IBizAPI.java
+++ kiwi-word/kiwi-word-api/src/main/java/me/fengorz/kiwi/word/api/feign/IBizAPI.java
@@ -92,7 +92,7 @@ public interface IBizAPI {
     @PostMapping(WORD_FETCH_QUEUE + "/storePhrasesFetchResult")
     R<Boolean> storePhrasesFetchResult(@RequestBody FetchPhraseResultDTO dto);
 
-    @GetMapping(WORD_MAIN + "/query/{wordName}")
-    R<WordQueryVO> queryWord(@PathVariable String wordName);
+    @GetMapping(WORD_MAIN + "/query/gate/{keyword}")
+    R<WordQueryVO> queryWord(@PathVariable String keyword);
 
 }

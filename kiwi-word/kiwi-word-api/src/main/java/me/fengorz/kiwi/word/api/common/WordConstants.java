36d418e15e44b235897db3a5654ad88ff04cf8d2
coding-by-feng <1215587087@qq.com>
2020-12-23 11:26:16 +0800

    Modifying ES Query logic.

--- kiwi-word/kiwi-word-api/src/main/java/me/fengorz/kiwi/word/api/common/WordConstants.java
+++ kiwi-word/kiwi-word-api/src/main/java/me/fengorz/kiwi/word/api/common/WordConstants.java
@@ -67,5 +67,6 @@ public class WordConstants {
     public static final int REMEMBER_ARCHIVE_TYPE_EXAMPLE = 3;
 
     public static final String VO_PATH_MEANING_CHINESE = "characterVOList.paraphraseVOList.meaningChinese";
+    public static final String VO_PATH_EXAMPLE_CHINESE = "characterVOList.paraphraseVOList.exampleVOList.exampleTranslate";
 
 }

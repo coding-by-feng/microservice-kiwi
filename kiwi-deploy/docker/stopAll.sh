700b939e2b4d480cb4ae056fcc3b6bd0ccfc7168
coding-by-feng <1215587087@qq.com>
2021-06-26 16:19:35 +0800

    Add deploy shell.

---
+++ kiwi-deploy/docker/stopAll.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+killall autoCheckService.sh
+
+echo "stop container beginning"
+
+docker stop $(docker ps -a| grep kiwi-crawler | awk '{print $1}' )
+docker stop $(docker ps -a| grep kiwi-word-biz-crawler | awk '{print $1}' )
+docker stop $(docker ps -a| grep kiwi-word-biz | awk '{print $1}' )
+docker stop $(docker ps -a| grep kiwi-upms | awk '{print $1}' )
+docker stop $(docker ps -a| grep kiwi-auth | awk '{print $1}' )
+docker stop $(docker ps -a| grep kiwi-gate | awk '{print $1}' )
+docker stop $(docker ps -a| grep kiwi-config | awk '{print $1}' )
+docker stop $(docker ps -a| grep kiwi-eureka | awk '{print $1}' )
+
+echo "stop container success"

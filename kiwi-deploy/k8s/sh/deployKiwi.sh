fd6ac66c45f742014fad5224cd835f3b89cfe7c2
coding-by-feng <1215587087@qq.com>
2021-12-11 08:51:04 +0800

    Updated the version of Dockerfile and shell script.

--- kiwi-deploy/k8s/sh/deployKiwi.sh
+++ kiwi-deploy/k8s/sh/deployKiwi.sh
@@ -32,13 +32,13 @@ mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-crawler/Dockerfile ~/docker/kiwi/c
 mv -f ~/microservice-kiwi/kiwi-auth/Dockerfile ~/docker/kiwi/auth/
 mv -f ~/microservice-kiwi/kiwi-gateway/Dockerfile ~/docker/kiwi/gate/
 
-mv -f ~/.m2/repository/me/fengorz/kiwi-eureka/1.0-SNAPSHOT/kiwi-eureka-1.0-SNAPSHOT.jar ~/docker/kiwi/eureka/
-mv -f ~/.m2/repository/me/fengorz/kiwi-config/1.0-SNAPSHOT/kiwi-config-1.0-SNAPSHOT.jar ~/docker/kiwi/config/
-mv -f ~/.m2/repository/me/fengorz/kiwi-upms-biz/1.0-SNAPSHOT/kiwi-upms-biz-1.0-SNAPSHOT.jar ~/docker/kiwi/upms/
-mv -f ~/.m2/repository/me/fengorz/kiwi-auth/1.0-SNAPSHOT/kiwi-auth-1.0-SNAPSHOT.jar ~/docker/kiwi/auth/
-mv -f ~/.m2/repository/me/fengorz/kiwi-gateway/1.0-SNAPSHOT/kiwi-gateway-1.0-SNAPSHOT.jar ~/docker/kiwi/gate/
-mv -f ~/.m2/repository/me/fengorz/kiwi-word-biz/1.0-SNAPSHOT/kiwi-word-biz-1.0-SNAPSHOT.jar ~/docker/kiwi/word/
-mv -f ~/.m2/repository/me/fengorz/kiwi-word-crawler/1.0-SNAPSHOT/kiwi-word-crawler-1.0-SNAPSHOT.jar ~/docker/kiwi/crawler/
+mv -f ~/.m2/repository/me/fengorz/kiwi-eureka/1.0/kiwi-eureka-1.0.jar ~/docker/kiwi/eureka/
+mv -f ~/.m2/repository/me/fengorz/kiwi-config/1.0/kiwi-config-1.0.jar ~/docker/kiwi/config/
+mv -f ~/.m2/repository/me/fengorz/kiwi-upms-biz/1.0/kiwi-upms-biz-1.0.jar ~/docker/kiwi/upms/
+mv -f ~/.m2/repository/me/fengorz/kiwi-auth/1.0/kiwi-auth-1.0.jar ~/docker/kiwi/auth/
+mv -f ~/.m2/repository/me/fengorz/kiwi-gateway/1.0/kiwi-gateway-1.0.jar ~/docker/kiwi/gate/
+mv -f ~/.m2/repository/me/fengorz/kiwi-word-biz/1.0/kiwi-word-biz-1.0.jar ~/docker/kiwi/word/
+mv -f ~/.m2/repository/me/fengorz/kiwi-word-crawler/1.0/kiwi-word-crawler-1.0.jar ~/docker/kiwi/crawler/
 
 echo "docker build beginning"
 

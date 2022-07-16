9f453e7ca5aea01ebf13747440bffc99abf27a20
coding-by-feng <1215587087@qq.com>
2022-07-16 18:55:33 +0800

    feat(TTS): Add voicerss TTS function.

---
+++ kiwi-common/kiwi-common-tts/src/test/java/me/fengorz/kiwi/common/tts/service/impl/TtsServiceImplTest.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *   Copyright [2019~2025] [codingByFeng]
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *
+ */
+
+package me.fengorz.kiwi.common.tts.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import me.fengorz.kiwi.common.sdk.exception.tts.TtsException;
+import me.fengorz.kiwi.common.tts.service.TtsService;
+
+public class TtsServiceImplTest {
+
+    private final TtsService ttsService = new TtsServiceImpl();
+
+    @Test
+    public void test() {
+        assertThrows(TtsException.class, () -> ttsService.speechEnglish("02df0a8f48b641548ec4224c24ebff01", "hello world!"));
+    }
+
+}

78005c2c0a0d98d86755c5af0a8662b7f001d98c
coding-by-feng <1215587087@qq.com>
2022-07-24 04:08:57 +0800

    refactor(TTS): Add deprecateApiKeyToday function.

--- kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/service/operate/IReviewService.java
+++ kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/service/operate/IReviewService.java
@@ -65,4 +65,6 @@ public interface IReviewService {
     Integer queryTtsApiKeyUsed(String apiKey);
 
     Integer useTtsApiKey(String apiKey, Integer time);
+
+    void deprecateApiKeyToday(String apiKey);
 }

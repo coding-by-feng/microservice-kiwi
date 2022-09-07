30514fe244ffb913a8ae7f37dce49f57a04823fd
coding-by-feng <1215587087@qq.com>
2022-09-08 00:04:02 +0800

    refactor(*): The super epic optimization.

---
+++ kiwi-word/kiwi-word-crawler/src/main/java/me/fengorz/kiwi/dict/crawler/component/consumer/phrase/FetchRunUpConsumer.java
@@ -0,0 +1,75 @@
+/*
+ *
+ * Copyright [2019~2025] [codingByFeng]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *
+ */
+
+package me.fengorz.kiwi.dict.crawler.component.consumer.phrase;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.fengorz.kiwi.dict.crawler.component.consumer.base.AbstractConsumer;
+import me.fengorz.kiwi.dict.crawler.component.consumer.base.IConsumer;
+import me.fengorz.kiwi.dict.crawler.config.properties.CrawlerConfigProperties;
+import me.fengorz.kiwi.dict.crawler.service.IFetchService;
+import me.fengorz.kiwi.word.api.dto.queue.FetchPhraseRunUpMqDTO;
+
+/**
+ * @Author zhanshifeng @Date 2019/10/28 4:25 PM
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@RabbitListener(bindings = @QueueBinding(
+    value = @Queue(value = "${mq.config.phraseRunUpFromCambridge.fetchQueue}", autoDelete = "false"),
+    exchange = @Exchange(value = "${mq.config.phraseRunUpFromCambridge.exchange}"),
+    key = "${mq.config.phraseRunUpFromCambridge.fetchQueue}"))
+public class FetchRunUpConsumer extends AbstractConsumer<FetchPhraseRunUpMqDTO>
+    implements IConsumer<FetchPhraseRunUpMqDTO> {
+
+    private final IFetchService fetchService;
+    private final CrawlerConfigProperties crawlerConfigProperties;
+
+    @Resource(name = "fetchWordThreadExecutor")
+    private ThreadPoolTaskExecutor fetchWordThreadExecutor;
+
+    @PostConstruct
+    private void init() {
+        super.taskExecutor = this.fetchWordThreadExecutor;
+        super.maxPoolSize = this.crawlerConfigProperties.getMaxPoolSize();
+        super.startWorkLog = "rabbitMQ fetch one run-up of phrase is 【{}】";
+    }
+
+    @Override
+    @RabbitHandler
+    public void consume(FetchPhraseRunUpMqDTO dto) {
+        super.work(dto);
+    }
+
+    @Override
+    protected void execute(FetchPhraseRunUpMqDTO dto) {
+        fetchService.retrievePhrase(dto);
+    }
+
+    @Override
+    protected void errorCallback(FetchPhraseRunUpMqDTO dto, Exception e) {
+        // TODO ZSF 增加一个抓取队列状态恢复到待抓取的接口，防止数据抓取丢失
+    }
+}

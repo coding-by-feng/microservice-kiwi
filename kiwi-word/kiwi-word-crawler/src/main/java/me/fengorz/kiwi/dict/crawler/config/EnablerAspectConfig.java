5dfd12a96c63b5dbbc0049d2857b96db76bfa044
coding-by-feng <1215587087@qq.com>
2022-09-14 21:36:20 +0800

    fix(crawler): Fix bug for the super epic optimization.

--- kiwi-word/kiwi-word-crawler/src/main/java/me/fengorz/kiwi/dict/crawler/config/EnablerAspectConfig.java
+++ kiwi-word/kiwi-word-crawler/src/main/java/me/fengorz/kiwi/dict/crawler/config/EnablerAspectConfig.java
@@ -22,21 +22,21 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.extern.slf4j.Slf4j;
 import me.fengorz.kiwi.common.sdk.annotation.ScheduledAwake;
+import me.fengorz.kiwi.common.sdk.util.spring.SpringUtils;
 import me.fengorz.kiwi.dict.crawler.component.Enabler;
 
 @Slf4j
 @Aspect
 public class EnablerAspectConfig {
 
-    @Autowired
-    private Enabler enabler;
+    private final Enabler enabler;
 
     public EnablerAspectConfig() {
         log.info("EnablerAspectConfig set up.");
+        this.enabler = SpringUtils.getBean(Enabler.class);
     }
 
     @Pointcut("@annotation(me.fengorz.kiwi.common.mq.MqAwake) && within(me.fengorz.kiwi.dict.crawler..*)")
@@ -50,7 +50,7 @@ public class EnablerAspectConfig {
         log.info("mqAround");
         if (!enabler.isMqEnable()) {
             log.info("Enabler didn't enabled MQ.");
-            return null;
+            return new Object();
         }
         return point.proceed();
     }
@@ -58,12 +58,13 @@ public class EnablerAspectConfig {
     @Around("schedulePointcut()")
     public Object scheduleAround(ProceedingJoinPoint point) throws Throwable {
         log.info("scheduleAround");
+        String key;
         MethodSignature signature = (MethodSignature)point.getSignature();
         ScheduledAwake scheduledAwake = signature.getMethod().getAnnotation(ScheduledAwake.class);
-        String key = scheduledAwake.key();
+        key = scheduledAwake.key();
         if (StringUtils.isBlank(key) || !enabler.isSchedulerEnable(key)) {
             log.info("Enabler didn't enabled Scheduler.");
-            return null;
+            return new Object();
         }
         log.info("Enabler[{}] enabled Scheduler.", key);
         return point.proceed();

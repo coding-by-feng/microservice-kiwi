59dd8640bbdde6fbff07f730c60c1d56c55f5137
coding-by-feng <1215587087@qq.com>
2022-09-19 23:01:17 +0800

    feat(IELTS): 1. Added common paraphrase star list. 2. Refactored some other code logic.

--- kiwi-common/kiwi-common-api/src/main/java/me/fengorz/kiwi/common/api/feign/AbstractFallback.java
+++ kiwi-common/kiwi-common-api/src/main/java/me/fengorz/kiwi/common/api/feign/AbstractFallback.java
@@ -1,5 +1,6 @@
 /*
- * Copyright [2019~2025] [zhanshifeng]
+ *
+ * Copyright [2019~2025] [codingByFeng]
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -9,31 +10,35 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
+ *
+ *
  */
 
-package me.fengorz.kiwi.word.api.feign.fallback;
+package me.fengorz.kiwi.common.api.feign;
 
-import org.springframework.stereotype.Component;
-
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import me.fengorz.kiwi.common.api.R;
-import me.fengorz.kiwi.word.api.feign.IQueryAPI;
-import me.fengorz.kiwi.word.api.vo.detail.WordQueryVO;
 
 /**
- * Hystrix熔断回调实现 @Author zhanshifeng
- *
- * @date 2020-05-24 01:40:36
+ * @Description TODO
+ * @Author zhanshifeng
+ * @Date 2022/4/24 15:11
  */
 @Slf4j
-@Component
-public class QueryAPIFallback extends AbstractFallback implements IQueryAPI {
+public class AbstractFallback {
 
-    @Override
-    public R<Page<WordQueryVO>> queryWord(String wordName) {
+    @Setter
+    protected Throwable throwable;
+
+    protected <T> R<T> handleError() {
         log.error(throwable.getCause().getMessage());
         return R.feignCallFailed(throwable.getMessage());
     }
+
+    protected void handleErrorNotReturn() {
+        log.error(throwable.getCause().getMessage());
+        R.feignCallFailed(throwable.getMessage());
+    }
+
 }

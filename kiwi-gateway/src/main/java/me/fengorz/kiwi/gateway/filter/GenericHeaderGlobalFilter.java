aaae698a86b500418563785ec88d49577b4f459b
coding-by-feng <1215587087@qq.com>
2022-09-27 01:37:59 +0800

    re-fix(cache-control): Add header cache-control for all api response.

--- kiwi-gateway/src/main/java/me/fengorz/kiwi/gateway/filter/GenericHeaderGlobalFilter.java
+++ kiwi-gateway/src/main/java/me/fengorz/kiwi/gateway/filter/GenericHeaderGlobalFilter.java
@@ -16,21 +16,15 @@
 
 package me.fengorz.kiwi.gateway.filter;
 
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.CacheControl;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
-import cn.hutool.http.Header;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.fengorz.kiwi.gateway.property.CacheControlApiProperties;
 import reactor.core.publisher.Mono;
 
 /**
@@ -43,20 +37,26 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class GenericHeaderGlobalFilter implements GlobalFilter, Ordered {
 
-    private final CacheControlApiProperties cacheControlApiProperties;
-
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String path = exchange.getRequest().getURI().getPath();
         log.info("path is: {}", path);
 
-        if (StringUtils.startsWithAny(path, cacheControlApiProperties.getApi().toArray(new String[0]))) {
-            ServerHttpResponse response = exchange.getResponse();
-            log.info("Enabled cache-control header.");
-            response.getHeaders().add(Header.CACHE_CONTROL.toString(),
-                CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic().toString());
-            return chain.filter(exchange.mutate().response(response).build());
-        }
+        ServerHttpResponse response = exchange.getResponse();
+        response.getHeaders().forEach((name, value) -> log.info("Header name: {}, value: {}", name, value));
+
+        // if (StringUtils.startsWithAny(path, cacheControlApiProperties.getApi().toArray(new String[0]))) {
+        // ServerHttpResponse response = exchange.getResponse();
+        // log.info("Enabled cache-control header.");
+        // response.getHeaders().remove(Header.CACHE_CONTROL.toString());
+        // response.getHeaders().remove(Header.PRAGMA.toString());
+        // response.getHeaders().remove(GlobalConstants.HEADERS.HEADER_EXPIRES_UPPER_CASE);
+        // response.getHeaders().remove(GlobalConstants.HEADERS.HEADER_EXPIRES_LOWER_CASE);
+        // response.getHeaders().add(Header.CACHE_CONTROL.toString(),
+        // CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic().toString());
+        //
+        // return chain.filter(exchange.mutate().response(response).build());
+        // }
 
         return chain.filter(exchange);
     }
--- kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/config/WebSecurityConfig.java
+++ kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/config/WebSecurityConfig.java
@@ -16,28 +16,46 @@
 
 package me.fengorz.kiwi.word.biz.config;
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.CacheControl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
+import cn.hutool.http.Header;
 import lombok.extern.slf4j.Slf4j;
+import me.fengorz.kiwi.common.sdk.util.validate.KiwiAssertUtils;
+import me.fengorz.kiwi.word.biz.property.CacheControlApiProperties;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
+// @EnableGlobalMethodSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements Ordered {
 
-    public WebSecurityConfig() {
+    private final CacheControlApiProperties cacheControlApiProperties;
+
+    public WebSecurityConfig(CacheControlApiProperties cacheControlApiProperties) {
         // default enable: false
         super(false);
+        KiwiAssertUtils.serviceNotEmpty(cacheControlApiProperties, "cacheControlApiProperties must not be empty.");
+        this.cacheControlApiProperties = cacheControlApiProperties;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         log.info("Disable default header.");
-        http.headers().cacheControl().disable();
+        http.requestMatcher(request -> StringUtils
+            .startsWithAny(request.getRequestURI(), cacheControlApiProperties.getNeedCacheApi().toArray(new String[0])))
+            .headers().cacheControl().disable()
+            .addHeaderWriter(new StaticHeadersWriter(Header.CACHE_CONTROL.toString(),
+                CacheControl.maxAge(3, TimeUnit.DAYS).mustRevalidate().cachePublic().getHeaderValue()))
+            .and().csrf().disable();
     }
 
     @Override

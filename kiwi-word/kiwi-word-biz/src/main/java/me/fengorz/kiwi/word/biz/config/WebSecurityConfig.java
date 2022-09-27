360c924882e83d2f6de2933a7b787c1d970313b9
coding-by-feng <1215587087@qq.com>
2022-09-27 11:59:36 +0800

    re-fix(cache-control): Add header cache-control for all api response.

--- kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/config/WebSecurityConfig.java
+++ kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements O
             .startsWithAny(request.getRequestURI(), cacheControlApiProperties.getNeedCacheApi().toArray(new String[0])))
             .headers().cacheControl().disable()
             .addHeaderWriter(new StaticHeadersWriter(Header.CACHE_CONTROL.toString(),
-                CacheControl.maxAge(3, TimeUnit.DAYS).mustRevalidate().cachePublic().getHeaderValue()))
+                CacheControl.maxAge(3, TimeUnit.DAYS).cachePublic().getHeaderValue()))
             .and().csrf().disable();
     }
 

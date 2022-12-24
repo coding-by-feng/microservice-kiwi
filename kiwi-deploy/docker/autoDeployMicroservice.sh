9b18383ccbad7e47979bb0cad2ead0b4a926951a
coding-by-feng <1215587087@qq.com>
2022-12-24 22:10:06 +0800

    refactor(deploy): Refactor the ow(only-word-service) mode for deploy script.

--- kiwi-deploy/docker/autoDeployMicroservice.sh
+++ kiwi-deploy/docker/autoDeployMicroservice.sh
@@ -4,39 +4,63 @@
 
 # systemctl restart docker
 
-rm -rf ~/docker/kiwi/eureka/logs/*
-rm -rf ~/docker/kiwi/config/logs/*
-rm -rf ~/docker/kiwi/upms/logs/*
-rm -rf ~/docker/kiwi/auth/logs/*
-rm -rf ~/docker/kiwi/gate/logs/*
-rm -rf ~/docker/kiwi/word/logs/*
-rm -rf ~/docker/kiwi/word/logs_02/*
-rm -rf ~/docker/kiwi/word/crawlerTmp/*
-rm -rf ~/docker/kiwi/word/crawlerTmp_02/*
-rm -rf ~/docker/kiwi/word/bizTmp/*
-rm -rf ~/docker/kiwi/word/bizTmp_02/*
-rm -rf ~/docker/kiwi/crawler/logs/*
+if [ $1 == "ow" ]
+then
+  rm -rf ~/docker/kiwi/word/logs/*
+  rm -rf ~/docker/kiwi/word/logs_02/*
+  rm -rf ~/docker/kiwi/word/crawlerTmp/*
+  rm -rf ~/docker/kiwi/word/crawlerTmp_02/*
+  rm -rf ~/docker/kiwi/word/bizTmp/*
+  rm -rf ~/docker/kiwi/word/bizTmp_02/*
+  rm -rf ~/docker/kiwi/crawler/logs/*
+else
+  rm -rf ~/docker/kiwi/eureka/logs/*
+  rm -rf ~/docker/kiwi/config/logs/*
+  rm -rf ~/docker/kiwi/upms/logs/*
+  rm -rf ~/docker/kiwi/auth/logs/*
+  rm -rf ~/docker/kiwi/gate/logs/*
+  rm -rf ~/docker/kiwi/word/logs/*
+  rm -rf ~/docker/kiwi/word/logs_02/*
+  rm -rf ~/docker/kiwi/word/crawlerTmp/*
+  rm -rf ~/docker/kiwi/word/crawlerTmp_02/*
+  rm -rf ~/docker/kiwi/word/bizTmp/*
+  rm -rf ~/docker/kiwi/word/bizTmp_02/*
+  rm -rf ~/docker/kiwi/crawler/logs/*
+fi
 
 mvn clean install -Dmaven.test.skip=true
 
-mv -f ~/microservice-kiwi/kiwi-eureka/Dockerfile ~/docker/kiwi/eureka/
-mv -f ~/microservice-kiwi/kiwi-config/Dockerfile ~/docker/kiwi/config/
-mv -f ~/microservice-kiwi/kiwi-upms/kiwi-upms-biz/Dockerfile ~/docker/kiwi/upms/
-mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/docker/biz/Dockerfile ~/docker/kiwi/word/biz
-mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/docker/crawler/Dockerfile ~/docker/kiwi/word/crawler
-mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-crawler/Dockerfile ~/docker/kiwi/crawler/
-mv -f ~/microservice-kiwi/kiwi-auth/Dockerfile ~/docker/kiwi/auth/
-mv -f ~/microservice-kiwi/kiwi-gateway/Dockerfile ~/docker/kiwi/gate/
+if [ $1 == "ow" ]
+then
+  mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/docker/biz/Dockerfile ~/docker/kiwi/word/biz
+  mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/docker/crawler/Dockerfile ~/docker/kiwi/word/crawler
+  mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-crawler/Dockerfile ~/docker/kiwi/crawler/
+else
+  mv -f ~/microservice-kiwi/kiwi-eureka/Dockerfile ~/docker/kiwi/eureka/
+  mv -f ~/microservice-kiwi/kiwi-config/Dockerfile ~/docker/kiwi/config/
+  mv -f ~/microservice-kiwi/kiwi-upms/kiwi-upms-biz/Dockerfile ~/docker/kiwi/upms/
+  mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/docker/biz/Dockerfile ~/docker/kiwi/word/biz
+  mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/docker/crawler/Dockerfile ~/docker/kiwi/word/crawler
+  mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-crawler/Dockerfile ~/docker/kiwi/crawler/
+  mv -f ~/microservice-kiwi/kiwi-auth/Dockerfile ~/docker/kiwi/auth/
+  mv -f ~/microservice-kiwi/kiwi-gateway/Dockerfile ~/docker/kiwi/gate/
+fi
 
-mv -f ~/.m2/repository/me/fengorz/kiwi-eureka/1.0/kiwi-eureka-1.0.jar ~/docker/kiwi/eureka/
-mv -f ~/.m2/repository/me/fengorz/kiwi-config/1.0/kiwi-config-1.0.jar ~/docker/kiwi/config/
-mv -f ~/.m2/repository/me/fengorz/kiwi-upms-biz/1.0/kiwi-upms-biz-1.0.jar ~/docker/kiwi/upms/
-mv -f ~/.m2/repository/me/fengorz/kiwi-auth/1.0/kiwi-auth-1.0.jar ~/docker/kiwi/auth/
-mv -f ~/.m2/repository/me/fengorz/kiwi-gateway/1.0/kiwi-gateway-1.0.jar ~/docker/kiwi/gate/
-mv -f ~/.m2/repository/me/fengorz/kiwi-word-biz/1.0/kiwi-word-biz-1.0.jar ~/docker/kiwi/word/
-mv -f ~/.m2/repository/me/fengorz/kiwi-word-crawler/1.0/kiwi-word-crawler-1.0.jar ~/docker/kiwi/crawler/
+if [ $1 == "ow" ]
+then
+  mv -f ~/.m2/repository/me/fengorz/kiwi-word-biz/1.0/kiwi-word-biz-1.0.jar ~/docker/kiwi/word/
+  mv -f ~/.m2/repository/me/fengorz/kiwi-word-crawler/1.0/kiwi-word-crawler-1.0.jar ~/docker/kiwi/crawler/
+else
+  mv -f ~/.m2/repository/me/fengorz/kiwi-eureka/1.0/kiwi-eureka-1.0.jar ~/docker/kiwi/eureka/
+  mv -f ~/.m2/repository/me/fengorz/kiwi-config/1.0/kiwi-config-1.0.jar ~/docker/kiwi/config/
+  mv -f ~/.m2/repository/me/fengorz/kiwi-upms-biz/1.0/kiwi-upms-biz-1.0.jar ~/docker/kiwi/upms/
+  mv -f ~/.m2/repository/me/fengorz/kiwi-auth/1.0/kiwi-auth-1.0.jar ~/docker/kiwi/auth/
+  mv -f ~/.m2/repository/me/fengorz/kiwi-gateway/1.0/kiwi-gateway-1.0.jar ~/docker/kiwi/gate/
+  mv -f ~/.m2/repository/me/fengorz/kiwi-word-biz/1.0/kiwi-word-biz-1.0.jar ~/docker/kiwi/word/
+  mv -f ~/.m2/repository/me/fengorz/kiwi-word-crawler/1.0/kiwi-word-crawler-1.0.jar ~/docker/kiwi/crawler/
+fi
 
-~/microservice-kiwi/kiwi-deploy/docker/autoDeploy.sh
+~/microservice-kiwi/kiwi-deploy/docker/autoDeploy.sh $1
 
 echo sleep 200
 sleep 200s

de82fec8991a8d6c7e708d7504ec11afdd324e9b
coding-by-feng <1215587087@qq.com>
2022-12-28 22:30:09 +0800

    feat(sql): Add kiwi-sql module.

---
+++ kiwi-sql/truncateAll.sql
@@ -0,0 +1,18 @@
+show tables;
+truncate word_fetch_queue;
+
+truncate word_character;
+truncate word_example_star_list;
+truncate word_example_star_rel;
+truncate word_main;
+truncate word_main_variant;
+truncate word_paraphrase;
+truncate word_paraphrase_example;
+truncate word_paraphrase_star_rel;
+truncate word_paraphrase_phrase;
+truncate word_pronunciation;
+truncate word_star_list;
+truncate word_star_rel;
+truncate word_paraphrase_star_list;
+
+

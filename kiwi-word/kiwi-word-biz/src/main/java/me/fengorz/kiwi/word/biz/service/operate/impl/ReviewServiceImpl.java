ab4861649f8f17c7283c54736df110583e9f3e7b
coding-by-feng <1215587087@qq.com>
2024-03-03 19:35:28 +0800

    fix(review): Disabled the cache config of Mybatis-Plus.

--- kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/service/operate/impl/ReviewServiceImpl.java
+++ kiwi-word/kiwi-word-biz/src/main/java/me/fengorz/kiwi/word/biz/service/operate/impl/ReviewServiceImpl.java
@@ -138,7 +138,6 @@ public class ReviewServiceImpl implements ReviewService {
         }
     }
 
-    @Async
     @Override
     @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
     public void increase(int type, Integer userId) {

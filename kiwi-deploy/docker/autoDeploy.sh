6eaf5a1e139a09bfea46ec888697a045eee06433
coding-by-feng <1215587087@qq.com>
2025-03-03 06:46:15 +1300

    refactor: Deleted unuseful parameter of deployment shell.

--- kiwi-deploy/docker/autoDeploy.sh
+++ kiwi-deploy/docker/autoDeploy.sh
@@ -3,65 +3,41 @@
 
 echo "delete container beginning"
 
-if [ "$1" == "-ow" ]
-then
-  docker rm -f  `docker ps -a| grep kiwi-word-biz | awk '{print $1}' `
-  docker rm -f  `docker ps -a| grep kiwi-crawler | awk '{print $1}' `
-else
-  docker rm -f  `docker ps -a| grep kiwi-eureka | awk '{print $1}' `
-  docker rm -f  `docker ps -a| grep kiwi-config | awk '{print $1}' `
-  docker rm -f  `docker ps -a| grep kiwi-gate | awk '{print $1}' `
-  docker rm -f  `docker ps -a| grep kiwi-upms | awk '{print $1}' `
-  docker rm -f  `docker ps -a| grep kiwi-auth | awk '{print $1}' `
-  docker rm -f  `docker ps -a| grep kiwi-word-biz | awk '{print $1}' `
-  docker rm -f  `docker ps -a| grep kiwi-crawler | awk '{print $1}' `
-fi
+docker rm -f  `docker ps -a| grep kiwi-eureka | awk '{print $1}' `
+docker rm -f  `docker ps -a| grep kiwi-config | awk '{print $1}' `
+docker rm -f  `docker ps -a| grep kiwi-gate | awk '{print $1}' `
+docker rm -f  `docker ps -a| grep kiwi-upms | awk '{print $1}' `
+docker rm -f  `docker ps -a| grep kiwi-auth | awk '{print $1}' `
+docker rm -f  `docker ps -a| grep kiwi-word-biz | awk '{print $1}' `
+docker rm -f  `docker ps -a| grep kiwi-crawler | awk '{print $1}' `
 
 echo "delete image beginning"
 
-if [ "$1" == "-ow" ]
-then
-  docker rmi kiwi-word-biz:2.0
-  docker rmi kiwi-crawler:2.0
-else
-  docker rmi kiwi-eureka:2.0
-  docker rmi kiwi-config:2.0
-  docker rmi kiwi-upms:2.0
-  docker rmi kiwi-auth:2.0
-  docker rmi kiwi-gate:2.0
-  docker rmi kiwi-word-biz:2.0
-  docker rmi kiwi-crawler:2.0
-fi
+docker rmi kiwi-eureka:2.0
+docker rmi kiwi-config:2.0
+docker rmi kiwi-upms:2.0
+docker rmi kiwi-auth:2.0
+docker rmi kiwi-gate:2.0
+docker rmi kiwi-word-biz:2.0
+docker rmi kiwi-crawler:2.0
 
 echo "docker build beginning"
 
-if [ "$1" == "-ow" ]
-then
-  docker build -f ~/docker/kiwi/word/biz/Dockerfile -t kiwi-word-biz:2.0 ~/docker/kiwi/word/
-  docker build -f ~/docker/kiwi/crawler/Dockerfile -t kiwi-crawler:2.0 ~/docker/kiwi/crawler/
-else
-  docker build -f ~/docker/kiwi/eureka/Dockerfile -t kiwi-eureka:2.0 ~/docker/kiwi/eureka/
-  docker build -f ~/docker/kiwi/config/Dockerfile -t kiwi-config:2.0 ~/docker/kiwi/config/
-  docker build -f ~/docker/kiwi/upms/Dockerfile -t kiwi-upms:2.0 ~/docker/kiwi/upms/
-  docker build -f ~/docker/kiwi/auth/Dockerfile -t kiwi-auth:2.0 ~/docker/kiwi/auth/
-  docker build -f ~/docker/kiwi/gate/Dockerfile -t kiwi-gate:2.0 ~/docker/kiwi/gate/
-  docker build -f ~/docker/kiwi/word/biz/Dockerfile -t kiwi-word-biz:2.0 ~/docker/kiwi/word/
-  docker build -f ~/docker/kiwi/crawler/Dockerfile -t kiwi-crawler:2.0 ~/docker/kiwi/crawler/
-fi
+docker build -f ~/docker/kiwi/eureka/Dockerfile -t kiwi-eureka:2.0 ~/docker/kiwi/eureka/
+docker build -f ~/docker/kiwi/config/Dockerfile -t kiwi-config:2.0 ~/docker/kiwi/config/
+docker build -f ~/docker/kiwi/upms/Dockerfile -t kiwi-upms:2.0 ~/docker/kiwi/upms/
+docker build -f ~/docker/kiwi/auth/Dockerfile -t kiwi-auth:2.0 ~/docker/kiwi/auth/
+docker build -f ~/docker/kiwi/gate/Dockerfile -t kiwi-gate:2.0 ~/docker/kiwi/gate/
+docker build -f ~/docker/kiwi/word/biz/Dockerfile -t kiwi-word-biz:2.0 ~/docker/kiwi/word/
+docker build -f ~/docker/kiwi/crawler/Dockerfile -t kiwi-crawler:2.0 ~/docker/kiwi/crawler/
 
 echo "podman-compose base beginning"
 
-if [ "$1" == "-ow" ]
-then
-  echo "podman-compose only-word-service starting"
-  podman-compose -f ~/microservice-kiwi/kiwi-deploy/docker/podman-compose-only-word-service.yml up -d
-else
-  podman-compose -f ~/microservice-kiwi/kiwi-deploy/docker/podman-compose-base.yml up -d
-  echo "success wait 100s"
-  sleep 100s
-  echo "podman-compose service beginning"
-  podman-compose -f ~/microservice-kiwi/kiwi-deploy/docker/podman-compose-service.yml up -d
-fi
+podman-compose -f ~/microservice-kiwi/kiwi-deploy/docker/podman-compose-base.yml up -d
+echo "success wait 100s"
+sleep 100s
+echo "podman-compose service beginning"
+podman-compose -f ~/microservice-kiwi/kiwi-deploy/docker/podman-compose-service.yml up -d
 
 docker stop `docker ps -a| grep kiwi-crawler | awk '{print $1}' `
 
--- kiwi-deploy/docker/deployKiwi.sh
+++ kiwi-deploy/docker/deployKiwi.sh
@@ -28,4 +28,42 @@ git pull
 
 chmod 777 ~/microservice-kiwi/kiwi-deploy/docker/*.sh
 
-~/microservice-kiwi/kiwi-deploy/docker/autoDeployMicroservice.sh
+rm -rf ~/docker/kiwi/eureka/logs/*
+rm -rf ~/docker/kiwi/config/logs/*
+rm -rf ~/docker/kiwi/upms/logs/*
+rm -rf ~/docker/kiwi/auth/logs/*
+rm -rf ~/docker/kiwi/gate/logs/*
+rm -rf ~/docker/kiwi/word/logs/*
+rm -rf ~/docker/kiwi/word/crawlerTmp/*
+rm -rf ~/docker/kiwi/word/bizTmp/*
+rm -rf ~/docker/kiwi/crawler/logs/*
+
+# Check if -skip-mvn parameter is provided, if not, run mvn clean install
+if [ "$1" != "-skip-mvn" ]
+then
+  mvn clean install -Dmaven.test.skip=true -B
+fi
+
+mv -f ~/microservice-kiwi/kiwi-eureka/Dockerfile ~/docker/kiwi/eureka/
+mv -f ~/microservice-kiwi/kiwi-config/Dockerfile ~/docker/kiwi/config/
+mv -f ~/microservice-kiwi/kiwi-upms/kiwi-upms-biz/Dockerfile ~/docker/kiwi/upms/
+mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/docker/biz/Dockerfile ~/docker/kiwi/word/biz
+mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-biz/docker/crawler/Dockerfile ~/docker/kiwi/word/crawler
+mv -f ~/microservice-kiwi/kiwi-word/kiwi-word-crawler/Dockerfile ~/docker/kiwi/crawler/
+mv -f ~/microservice-kiwi/kiwi-auth/Dockerfile ~/docker/kiwi/auth/
+mv -f ~/microservice-kiwi/kiwi-gateway/Dockerfile ~/docker/kiwi/gate/
+
+mv -f ~/.m2/repository/me/fengorz/kiwi-eureka/2.0/kiwi-eureka-2.0.jar ~/docker/kiwi/eureka/
+mv -f ~/.m2/repository/me/fengorz/kiwi-config/2.0/kiwi-config-2.0.jar ~/docker/kiwi/config/
+mv -f ~/.m2/repository/me/fengorz/kiwi-upms-biz/2.0/kiwi-upms-biz-2.0.jar ~/docker/kiwi/upms/
+mv -f ~/.m2/repository/me/fengorz/kiwi-auth/2.0/kiwi-auth-2.0.jar ~/docker/kiwi/auth/
+mv -f ~/.m2/repository/me/fengorz/kiwi-gateway/2.0/kiwi-gateway-2.0.jar ~/docker/kiwi/gate/
+mv -f ~/.m2/repository/me/fengorz/kiwi-word-biz/2.0/kiwi-word-biz-2.0.jar ~/docker/kiwi/word/
+mv -f ~/.m2/repository/me/fengorz/kiwi-word-crawler/2.0/kiwi-word-crawler-2.0.jar ~/docker/kiwi/crawler/
+
+~/microservice-kiwi/kiwi-deploy/docker/autoDeploy.sh $1
+
+echo sleep 200
+sleep 200s
+
+nohup ~/microservice-kiwi/kiwi-deploy/docker/autoCheckService.sh  >~/autoCheck.log 2>&1 &

0bf374dfae36bb14a85d435043358a28b57c714d
coding-by-feng <1215587087@qq.com>
2025-03-18 06:25:59 +1300

    refactor[deployment]: Optimized autoCheckService.sh and Dockerfile.

--- kiwi-deploy/docker/autoCheckService.sh
+++ kiwi-deploy/docker/autoCheckService.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-runningCode='200'
+# Define constants
+RUNNING_CODE='200'
+CURL_TIMEOUT=10
+CONTAINER_NAME='kiwi-service_kiwi-crawler_1'
+SERVICE_URL='http://kiwi-crawler:6001/actuator/info'
+LONG_SLEEP=60
+SHORT_SLEEP=15
+
+# Log function to standardize output
+log_message() {
+    echo "$(date "+%Y-%m-%d %H:%M:%S") $1"
+}
+
 while true
 do
-        crawlerCode=$(curl -I -m 10 -o /dev/null -s -w %{http_code} http://kiwi-crawler:6001/actuator/info)
-        if [ $crawlerCode != $runningCode ]
-        then
-                time=$(date "+%Y-%m-%d %H:%M:%S")
-                docker container start `docker ps -a| grep docker_kiwi-crawler_1 | awk '{print $1}' `
-                echo $time Word-Crawler Service has run just now, sleep
-                sleep 60s
+    # Get the HTTP status code with timeout protection
+    crawler_code=$(curl -I -m ${CURL_TIMEOUT} -o /dev/null -s -w %{http_code} ${SERVICE_URL})
+
+    # Check if the service needs to be restarted
+    if [ "$crawler_code" != "$RUNNING_CODE" ]; then
+        log_message "Word-Crawler Service is down (code: ${crawler_code}), restarting..."
+
+        # Get the container ID and restart it
+        container_id=$(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1}')
+
+        # Check if we found a container ID
+        if [ -n "$container_id" ]; then
+            docker container start "$container_id"
+            log_message "Word-Crawler Service has been restarted, sleeping for ${LONG_SLEEP}s"
         else
-                time=$(date "+%Y-%m-%d %H:%M:%S")
-                echo $time Word-Crawler Service is running, code = $code
-                sleep 15s
+            log_message "ERROR: Could not find container ${CONTAINER_NAME}"
         fi
+
+        sleep ${LONG_SLEEP}
+    else
+        log_message "Word-Crawler Service is running, code = ${crawler_code}"
+        sleep ${SHORT_SLEEP}
+    fi
 done

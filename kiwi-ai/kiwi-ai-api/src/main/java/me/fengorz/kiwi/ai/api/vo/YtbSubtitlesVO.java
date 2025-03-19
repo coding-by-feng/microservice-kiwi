42ba390d1405823314b7565517d0646e90d419e9
coding-by-feng <1215587087@qq.com>
2025-03-19 18:03:59 +1300

    refactor[YTB]: Optimized YouTube API.

--- kiwi-ai/kiwi-ai-api/src/main/java/me/fengorz/kiwi/ai/api/vo/YtbSubtitlesVO.java
+++ kiwi-ai/kiwi-ai-api/src/main/java/me/fengorz/kiwi/ai/api/vo/YtbSubtitlesVO.java
@@ -1,6 +1,7 @@
 package me.fengorz.kiwi.ai.api.vo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class YtbSubtitlesVO implements Serializable {
 
     private static final long serialVersionUID = 8961036140529222053L;

0e44bcc06dac6d9d90d0060b4165953100ca6250
coding-by-feng <1215587087@qq.com>
2025-04-03 20:22:38 +1300

    fix[YTB]: Fixed starting error.

--- kiwi-ai/kiwi-ai-biz/src/main/java/me/fengorz/kiwi/ai/config/AsyncConfiguration.java
+++ kiwi-ai/kiwi-ai-biz/src/main/java/me/fengorz/kiwi/ai/config/AsyncConfiguration.java
@@ -1,7 +1,6 @@
 package me.fengorz.kiwi.ai.config;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
@@ -14,7 +13,6 @@ import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-@ConditionalOnProperty(name = "youtube.video.batch.enabled", havingValue = "true")
 public class AsyncConfiguration implements AsyncConfigurer {
 
     private final ThreadExecutorProperties properties;

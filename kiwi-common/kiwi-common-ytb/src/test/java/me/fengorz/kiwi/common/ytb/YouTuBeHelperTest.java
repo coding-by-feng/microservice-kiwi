3372c819dde14a37834b4c864f9495e4b00371e3
coding-by-feng <1215587087@qq.com>
2025-04-04 21:03:43 +1300

    fix[YTB]: Skip the warning output while extracting the title of YTB video.

--- kiwi-common/kiwi-common-ytb/src/test/java/me/fengorz/kiwi/common/ytb/YouTuBeHelperTest.java
+++ kiwi-common/kiwi-common-ytb/src/test/java/me/fengorz/kiwi/common/ytb/YouTuBeHelperTest.java
@@ -27,6 +27,15 @@ class YouTuBeHelperTest {
     }
 
     @Test
+    void testTitle() throws Exception {
+        String videoTitle = new YouTuBeHelper().getVideoTitle("https://www.youtube.com/watch?v=hS-KO9RRmSc");
+
+        // Assert
+        assertEquals("Nintendo Switch 2 Features Unveiled at Nintendo Direct: Everything Announced in 15 Minutes", videoTitle);
+    }
+
+    @Test
+    @Disabled
     void extractAllVideoLinks_Success() throws Exception {
         // Arrange
         String channelUrl = "https://www.youtube.com/c/ScientificAmerican";

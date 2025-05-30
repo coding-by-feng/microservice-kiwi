914fb5f67f3eb6c25c8d17bc3bb947e87f82292e
coding-by-feng <1215587087@qq.com>
2025-05-30 14:54:44 +1200

    build[deployment]: Completed the epic deployment scripts.

--- kiwi-deploy/kiwi-ui/auto_deploy_ui.sh
+++ kiwi-deploy/kiwi-ui/auto_deploy_ui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~/docker/ui/ && mv -f /home/kiwi_s_f_zhan/dist.zip ./ && rm -rf dist __MACOSX/ && unzip dist.zip && chmod -R 777 dist && docker restart kiwi-ui && echo "Deployment completed successfully!"
+cd ~/docker/ui/ && mv -f ~/dist.zip ./ && rm -rf dist __MACOSX/ && unzip dist.zip && chmod -R 777 dist && docker restart kiwi-ui && echo "Deployment completed successfully!"

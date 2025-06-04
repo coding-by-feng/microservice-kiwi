37985c885abb2b8e75075ad1398c1f1687dc4d27
coding-by-feng <1215587087@qq.com>
2025-06-04 16:39:53 +1200

    Revert "refactor[AI]: Changed GET to POST for long url."
    This reverts commit a2473e30d3b223c97d0bfadd3780b9690ddeb7e8.

--- kiwi-deploy/local_scp.sh
+++ kiwi-deploy/local_scp.sh
@@ -1,11 +1,11 @@
 # Command 1 variables
-SOURCE_PATH_1="/Users/zhanshifeng/Documents/myDocument/idea-project/importance-back-up/kiwi-db.sql"
+SOURCE_PATH_1="~/Documents/myDocument/idea-project/importance-back-up/kiwi-db.sql"
 REMOTE_USER="kason"
-REMOTE_HOST="kason.local"
+REMOTE_HOST="kason-server.local"
 
 # Command 2 variables
 SOURCE_PATH_2="/Users/zhanshifeng/Documents/myDocument/webstorm-projects/kiwi-ui-dev/dist.zip"
 
 # Then use them in your scp commands
-scp -r "$SOURCE_PATH_1" "$REMOTE_USER@$REMOTE_HOST":~
+scp -r "$SOURCE_PATH_1" "$REMOTE_USER@$REMOTE_HOST":~/docker/mysql
 scp -r "$SOURCE_PATH_2" "$REMOTE_USER@$REMOTE_HOST":~

736fb8545a8b557193ab9ec97ba95cc5a9add2c9
coding-by-feng <1215587087@qq.com>
2025-10-16 15:49:58 +1300

    feat: Add liquibase supporting.

--- kiwi-tools/kiwi-tools-biz/src/main/java/me/fengorz/kiwi/tools/ToolsApplication.java
+++ kiwi-tools/kiwi-tools-biz/src/main/java/me/fengorz/kiwi/tools/ToolsApplication.java
@@ -13,4 +13,3 @@ public class ToolsApplication {
         SpringApplication.run(ToolsApplication.class, args);
     }
 }
-
--- kiwi-tools/kiwi-tools-biz/src/main/java/me/fengorz/kiwi/tools/exception/ToolsException.java
+++ kiwi-tools/kiwi-tools-biz/src/main/java/me/fengorz/kiwi/tools/exception/ToolsException.java
@@ -6,19 +6,19 @@ import org.springframework.http.HttpStatus;
 import java.util.Map;
 
 @Getter
-public class AppException extends RuntimeException {
+public class ToolsException extends RuntimeException {
     private final HttpStatus status;
     private final String code;
     private final Map<String, Object> details;
 
-    public AppException(HttpStatus status, String code, String message) {
+    public ToolsException(HttpStatus status, String code, String message) {
         super(message);
         this.status = status;
         this.code = code;
         this.details = null;
     }
 
-    public AppException(HttpStatus status, String code, String message, Map<String, Object> details) {
+    public ToolsException(HttpStatus status, String code, String message, Map<String, Object> details) {
         super(message);
         this.status = status;
         this.code = code;
